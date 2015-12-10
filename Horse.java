bcca40233070ce51f9fcd383daa7f5c801d13915
simeon-nikolov <simeon.v.nikolov@gmail.com>
2015-12-10 19:16:18 +0200

    Refactorirah malko koda. Promenih nqkoi imena na nqkoi promenlivi. Izvedoh inicializaciqta na matricata v nov method. Preimenuvah imeto na methoda dots() na fillDots() tyj kato imenata na metodite e hubavo da bydat izvyr6vane na nqkakvo deistvie. Iztrih proverkite predi recursivnoto izvikvane vyv fillDots/dots, tyj kato te sa izli6ni - proverkata za dyno na rekursiq 6te hvane tezi slu4ai i taka koda stava po lesen za 4etene. Slojih proverkata dali se izliza ot matricata predi proverkata za == '.', za da ne dava exception pri opit da dostypi ne6to, koeto e izvyn masiva. Malko pipnah metoda printBorders, taka 4e da syzdade ramkite za celiq masiv - v for-ciklite tr da e do dyljinata na noviq masiv (novata matrica, koqto se syzdava i 6te sydyrja i borderite).

--- Horse.java
+++ Horse.java
@@ -1,106 +1,111 @@
-import java.util.Scanner;
 
 public class Horse {
 
 	public static void main(String[] args) {
-		System.out.println("Enter number of rows: ");
-		Scanner sc = new Scanner(System.in);
-		int row = sc.nextInt();
-		System.out.println("Enter number of columns: ");
-		int col = sc.nextInt();
-		char[][] real = new char[row][col];
-		 for(int rowInit = 0; rowInit < real.length; rowInit++){
-		 for (int colInit = 0; colInit< real[rowInit].length; colInit++){
-		 real[rowInit][colInit] = ' ';
-		 }
-		 }
-		System.out.println("Enter staring coordinares. Enter row number:");
-		int startRow = sc.nextInt();
-		while (startRow > row || startRow < 1) {
-			System.out.println("Enter valid row number:");
-			startRow = sc.nextInt();
-		}
-		System.out.println("Enter column number:");
-		int startCol = sc.nextInt();
-		while (startCol > col || startCol < 1) {
-			System.out.println("Enter valid column number:");
-			startCol = sc.nextInt();
-		}
-		startRow--;
-		startCol--;
-		dots(real, startRow, startCol);
-		print(real);
+//		System.out.println("Enter number of rows: ");
+//		Scanner sc = new Scanner(System.in);
+//		int rowsCount = sc.nextInt();
+//		System.out.println("Enter number of columns: ");
+//		int colsCount = sc.nextInt();
+//		char[][] board = new char[rowsCount][colsCount];
+//		
+//		initializeBoard(board);
+//		
+//		System.out.println("Enter starting coordinares. Enter row number:");
+//		int startRow = sc.nextInt();
+//		
+//		while (startRow > rowsCount || startRow < 1) {
+//			System.out.println("Enter valid row number:");
+//			startRow = sc.nextInt();
+//		}
+//		
+//		System.out.println("Enter column number:");
+//		int startCol = sc.nextInt();
+//		
+//		while (startCol > colsCount || startCol < 1) {
+//			System.out.println("Enter valid column number:");
+//			startCol = sc.nextInt();
+//		}
+//
+//		startRow--;
+//		startCol--;
+		
+		int rows = 2;
+		int cols = 4;
+		char[][] board = new char[rows][cols];
+		fillDots(board, 1, 1);
+		printBorders(board);
 
 	}
 
-	static void dots(char[][] x, int y, int z) {
-		if (x[y][z] == '.') {
+	private static void initializeBoard(char[][] board) {
+		for (int row = 0; row < board.length; row++) {
+			for (int col = 0; col < board[row].length; col++) {
+				board[row][col] = ' ';
+			}
+		}
+	}
+
+	static void fillDots(char[][] board, int coordX, int coordY) {
+		if (coordX < 0 || coordX >= board.length) {
 			return;
 		}
-		if (y < 0 || y >= x.length) {
+		
+		if (coordY < 0 || coordY >= board[coordX].length) {
 			return;
 		}
-		if (z < 0 || z >= x[y].length) {
+		
+		if (board[coordX][coordY] == '.') {
 			return;
 		}
-		x[y][z] = '.';
-		if (y - 2 >= 0) {
-			if (z - 1 >= 0) {
-				dots(x, y - 2, z - 1);
-			}
-			if (z + 1 < x[y].length) {
-				dots(x, y - 2, z + 1);
-			}
-		}
-		if (y - 1 >= 0) {
-			if (z - 2 >= 0) {
-				dots(x, y - 1, z - 2);
-			}
-			if (z + 2 < x[y].length) {
-				dots(x, y - 1, z + 2);
-			}
-		}
-		if (y + 2 < x.length) {
-			if (z - 1 >= 0) {
-				dots(x, y + 2, z - 1);
-			}
-			if (z + 1 < x[y].length) {
-				dots(x, y + 2, z + 1);
-			}
-		}
-		if (y + 1 < x.length) {
-			if (z - 2 >= 0) {
-				dots(x, y + 1, z - 2);
-			}
-			if (z + 2 < x[y].length) {
-				dots(x, y + 1, z + 2);
-			}
-		}
+		
+		board[coordX][coordY] = '.';
+		
+		fillDots(board, coordX - 2, coordY - 1);
+		fillDots(board, coordX - 2, coordY + 1);
+		fillDots(board, coordX - 1, coordY - 2);
+		fillDots(board, coordX - 1, coordY + 2);
+		fillDots(board, coordX + 2, coordY - 1);
+		fillDots(board, coordX + 2, coordY + 1);
+		fillDots(board, coordX + 1, coordY - 2);
+		fillDots(board, coordX + 1, coordY + 2);
 
 	}
 
-	static void print(char[][] x) {
+	static void print(char[][] x) {		
 		for (int row = 0; row < x.length; row++) {
 			for (int col = 0; col < x[row].length; col++) {
 				System.out.print(x[row][col] + " ");
 			}
+			
 			System.out.println();
 		}
+
 	}
-static void printBorders(char[][] x){
-	char[][] y = new char[x.length*2+1][x[0].length*2+1];
-	for (int row = 0; row < x.length; row+=2) {
-		for (int col = 1; col < x[row].length; col++) {
-			y[row][col] = '-';
-		}}
-	for (int row = 1; row < x.length; row+=2) {
-		for (int col = 0; col < x[row].length; col++) {
-			y[row][col] = '|';
-		}}
-	for (int row = 1; row < y.length; row+=2) {
-		for (int col = 1; col < y[row].length; col+=2) {
-			y[row][col] = x[row/2][col/2];
-			}}
-	print(y);
-}
+
+	static void printBorders(char[][] board) {
+		char[][] outputBoard = new char[board.length * 2 + 1][board[0].length * 2 + 1];
+		
+		initializeBoard(outputBoard);
+		
+		for (int row = 0; row < outputBoard.length; row += 2) {
+			for (int col = 0; col < outputBoard[row].length; col++) {
+				outputBoard[row][col] = '-';
+			}
+		}
+		
+		for (int row = 1; row < outputBoard.length; row += 2) {
+			for (int col = 0; col < outputBoard[row].length; col++) {
+				outputBoard[row][col] = '|';
+			}
+		}
+		
+		for (int row = 1; row < outputBoard.length; row += 2) {
+			for (int col = 1; col < outputBoard[row].length; col += 2) {
+				outputBoard[row][col] = board[row / 2][col / 2];
+			}
+		}
+		
+		print(outputBoard);
+	}
 }
